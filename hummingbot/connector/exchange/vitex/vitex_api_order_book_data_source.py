272c04ce59e81139d243085c9005e1a9c0533ffa
AJ. Cates <ajcates.com@gmail.com>
2021-03-31 15:39:54 +0000

    added a dirty hack to get the history command to work

--- hummingbot/connector/exchange/vitex/vitex_api_order_book_data_source.py
+++ hummingbot/connector/exchange/vitex/vitex_api_order_book_data_source.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+
 import asyncio
 import logging
 import pandas as pd
@@ -15,6 +16,7 @@ import ujson
 import websockets
 from websockets.exceptions import ConnectionClosed
 
+
 from hummingbot.connector.exchange.vitex.vitex_api import VitexAPI
 from hummingbot.core.data_type.order_book_tracker_data_source import OrderBookTrackerDataSource
 from hummingbot.core.data_type.order_book_message import OrderBookMessage
@@ -22,14 +24,19 @@ from hummingbot.core.data_type.order_book import OrderBook
 from hummingbot.logger import HummingbotLogger
 from hummingbot.connector.exchange.vitex.vitex_order_book import VitexOrderBook
 
+
 STREAM_URL = "wss://api.vitex.net/v2/ws"
+
+
 
 
 class VitexAPIOrderBookDataSource(OrderBookTrackerDataSource):
     MESSAGE_TIMEOUT = 30.0
     PING_TIMEOUT = 10.0
 
+
     _baobds_logger: Optional[HummingbotLogger] = None
+
 
     @classmethod
     def logger(cls) -> HummingbotLogger:
@@ -37,17 +44,22 @@ class VitexAPIOrderBookDataSource(OrderBookTrackerDataSource):
             cls._baobds_logger = logging.getLogger(__name__)
         return cls._baobds_logger
 
+
     def __init__(self, trading_pairs: List[str]):
         super().__init__(trading_pairs)
         self._order_book_create_function = lambda: OrderBook()
         self._vitex_api = VitexAPI()
+
 
     async def get_last_traded_prices(self, trading_pairs: List[str]) -> Dict[str, float]:
         symbols = VitexAPI.convert_to_exchange_trading_pair(",".join(trading_pairs))
         params = {
             "symbols": symbols
         }
-        response_data = await self._vitex_api.api_request("GET", "/ticker/24hr", params)
+        # ghetto hack
+        vxapi = VitexAPI()
+        response_data = await vxapi.api_request("GET", "/ticker/24hr", params)
+        # end ghetto hack.
         results = dict()
         for trading_pair in trading_pairs:
             resp_record = \
@@ -55,6 +67,7 @@ class VitexAPIOrderBookDataSource(OrderBookTrackerDataSource):
                     0]
             results[trading_pair] = float(resp_record["closePrice"])
         return results
+
 
     async def get_snapshot(self, trading_pair: str, limit: int = 1000) -> Dict[str, Any]:
         symbol = VitexAPI.convert_to_exchange_trading_pair(trading_pair)
@@ -66,8 +79,10 @@ class VitexAPIOrderBookDataSource(OrderBookTrackerDataSource):
             "symbol": symbol
         }
 
+
         result = await self._vitex_api.api_request("GET", "/depth", params)
         return result
+
 
     async def get_new_order_book(self, trading_pair: str) -> OrderBook:
         snapshot: Dict[str, Any] = await self.get_snapshot(trading_pair, 1000)
@@ -81,6 +96,7 @@ class VitexAPIOrderBookDataSource(OrderBookTrackerDataSource):
         order_book.apply_snapshot(snapshot_msg.bids, snapshot_msg.asks, snapshot_msg.update_id)
         return order_book
 
+
     @staticmethod
     async def fetch_trading_pairs() -> List[str]:
         try:
@@ -92,6 +108,7 @@ class VitexAPIOrderBookDataSource(OrderBookTrackerDataSource):
             pass
         return []
 
+
     async def _inner_messages(self,
                               ws: websockets.WebSocketClientProtocol) -> AsyncIterable[Any]:
         # Terminate the recv() loop as soon as the next message timed out, so the outer loop can reconnect.
@@ -102,8 +119,10 @@ class VitexAPIOrderBookDataSource(OrderBookTrackerDataSource):
                     msg_json = ujson.loads(msg)
                     event = msg_json["event"]
 
+
                     if event == "push":
                         yield msg_json
+
 
                 except asyncio.TimeoutError:
                     try:
@@ -119,6 +138,7 @@ class VitexAPIOrderBookDataSource(OrderBookTrackerDataSource):
         finally:
             await ws.close()
 
+
     async def listen_for_trades(self, ev_loop: asyncio.BaseEventLoop, output: asyncio.Queue):
         while True:
             try:
@@ -126,6 +146,7 @@ class VitexAPIOrderBookDataSource(OrderBookTrackerDataSource):
                 for pair in self._trading_pairs:
                     symbol = VitexAPI.convert_to_exchange_trading_pair(pair)
                     topics.append(f"market.{symbol}.trade")
+
 
                 async with websockets.connect(STREAM_URL) as ws:
                     ws: websockets.WebSocketClientProtocol = ws
@@ -145,6 +166,7 @@ class VitexAPIOrderBookDataSource(OrderBookTrackerDataSource):
                 self.logger().error("Unexpected error with WebSocket connection. Retrying after 30 seconds...",
                                     exc_info=True)
                 await asyncio.sleep(30.0)
+
 
     async def listen_for_order_book_diffs(self, ev_loop: asyncio.BaseEventLoop, output: asyncio.Queue):
         while True:
@@ -172,6 +194,7 @@ class VitexAPIOrderBookDataSource(OrderBookTrackerDataSource):
                 self.logger().error("Unexpected error with WebSocket connection. Retrying after 30 seconds...",
                                     exc_info=True)
                 await asyncio.sleep(30.0)
+
 
     async def listen_for_order_book_snapshots(self, ev_loop: asyncio.BaseEventLoop, output: asyncio.Queue):
         while True:
@@ -202,3 +225,4 @@ class VitexAPIOrderBookDataSource(OrderBookTrackerDataSource):
             except Exception:
                 self.logger().error("Unexpected error.", exc_info=True)
                 await asyncio.sleep(5.0)
+eep(5.0)
